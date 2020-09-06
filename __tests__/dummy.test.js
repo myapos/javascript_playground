209d4c2982048cbed52a5af83d4f570556b4a7e1
Myron Apostolakis <myron.apostolakis@sunlight.io>
2020-09-06 17:21:39 +0300

    added basic folder testing and babel

---
+++ __tests__/dummy.test.js
@@ -0,0 +1,7 @@
+const { iteratee } = require('lodash');
+
+describe('dummy', () => {
+  it('should be dummy', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ utils/__tests__/sum.test.js
@@ -0,0 +1,7 @@
+import 'sum' from '../sum';
+
+describe('sum', () => {
+  it('should be dummy', () => {
+    expect(true).toBe(true);
+  });
+});
