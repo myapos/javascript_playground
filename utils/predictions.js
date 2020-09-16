70895b2a36ea2b3deb6adea9118bb69c22887b00
Myron <myron.apostolakis@onapp.com>
2020-09-16 09:41:28 +0300

    tried adam

--- utils/predictions.js
+++ utils/predictions.js
@@ -25,22 +25,31 @@ const predictions = ({ midsX, midsY, filledDates, filledValues }) => {
   const toDates = filledDates.map((d) => createDate(d));
 
   const timestamps = convertToTimestaps(toDates);
+
   /* step 1 define a model */
   const model = tf.sequential();
-  model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+  model.add(tf.layers.dense({ units: 1, useBias: true, activation: 'linear', inputShape: [1] }));
 
-  // Prepare the model for training: Specify the loss and the optimizer.
-  model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+  // Prepare the model for training: Specify the loss and the optimizer. tf.train.sgd(0.1)
+  model.compile({ loss: 'meanSquaredError', optimizer: 'adam' });
 
-  // Generate some synthetic data for training.
+  model.summary();
+  // load data for training.
   const xs = tf.tensor2d(timestamps, [timestamps.length, 1]);
   const ys = tf.tensor2d(filledValues, [filledValues.length, 1]);
 
   //   Train the model using the data.
-  model.fit(xs, ys).then(() => {
-    // Use the model to do inference on a data point the model hasn't seen before:
-    model.predict(tf.tensor2d([5], [1, 1])).print();
-  });
+  model
+    .fit(xs, ys, {
+      epochs: 100,
+      callbacks: {
+        onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss= ${log.loss}`),
+      },
+    })
+    .then(() => {
+      // Use the model to do inference on a data point the model hasn't seen before:
+      model.predict(tf.tensor2d([5], [1, 1])).print();
+    });
 };
 
 export default predictions;
